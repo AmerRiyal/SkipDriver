8e1ad7da30ac0922045113fd2c53248fe9ab19d0
OmarNasrallah <omarnidal38@gmail.com>
2021-06-14 16:55:05 +0300

    adding notification

--- android/app/src/main/java/com/skipdriver/MainActivity.java
+++ android/app/src/main/java/com/skipdriver/MainActivity.java
@@ -1,5 +1,17 @@
 package com.skipdriver;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +24,11 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "skipdriver";
   }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    super.onCreate(savedInstanceState, persistentState);
+
+  }
+
 }
--- android/app/src/main/java/com/skipdriver/MainApplication.java
+++ android/app/src/main/java/com/skipdriver/MainApplication.java
@@ -2,6 +2,8 @@ package com.skipdriver;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -14,7 +16,17 @@ import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 import com.transistorsoft.rnbackgroundgeolocation.*;
 // import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 // import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -51,6 +63,21 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          NotificationChannel notificationChannel = new NotificationChannel("driverchannel", "skipdriver", NotificationManager.IMPORTANCE_HIGH);
+          notificationChannel.setShowBadge(true);
+          notificationChannel.setDescription("");
+          AudioAttributes att = new AudioAttributes.Builder()
+                  .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                  .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                  .build();
+          notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/notification"), att);
+          notificationChannel.enableVibration(true);
+          notificationChannel.setVibrationPattern(new long[]{400, 400});
+          notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+          NotificationManager manager = getSystemService(NotificationManager.class);
+          manager.createNotificationChannel(notificationChannel);
+      }
   }
 
   /**
